e600cf139235af3639c614d3d6d37b78e29b7388
Bryan Guamba <bryan.guamba@code-studios.com>
2015-12-18 19:27:29 -0500

    Reporte empleados

--- public/js/controllers/humanResources/employees/employeesReport.js
+++ public/js/controllers/humanResources/employees/employeesReport.js
@@ -3,20 +3,37 @@ angular.module('app').controller('EmployeesReportCtrl', [
   '$scope',
   '$modal',
   'DTOptionsBuilder',
-  'DTColumnBuilder',
   'server',
   'transferData',
   'optionsDataTable',
-  'dataTablePDFMaker',
-  'dataTableXLSXMaker',
-  function ($scope, $modal, DTOptionsBuilder, DTColumnBuilder, server, transferData, optionsDataTable, dataTablePDFMaker, dataTableXLSXMaker) {
+  function ($scope, $modal, DTOptionsBuilder, server, transferData, optionsDataTable) {
+
     handlePanelAction();
     $scope.panelExpand = '';
     $scope.isLoading = false;
     $scope.employees = [];
+    var title = 'Resumen Empleados';
+    var fileName = 'resumen_empleados' + moment().format('YYYYMMDD');
+    var tableInstance = {};
     var tableInstance = {};
 
     var fileName = 'ResumenEmpleados_' + moment().format('YYYYMMDD');
+
+    var editEmployee = function (selectedEmployee) {
+      var modalInstance = $modal.open({
+        templateUrl: '../../views/humanResources/employees/employeDetails.html',
+        controller: 'EditEmployeeCtrl',
+        windowClass: 'xlg',
+        resolve: {
+          selectedEmployee: function () {
+            return selectedEmployee;
+          }
+        }
+      });
+      modalInstance.result.then(function () {
+        reloadData();
+      });
+    };
 
     server.getAll('departments').success(function (data) {
       transferData.data.departments = data;
@@ -34,116 +51,36 @@ angular.module('app').controller('EmployeesReportCtrl', [
       transferData.data.maritalsStatus = data;
     });
 
-    $scope.tableColumns = [
-      DTColumnBuilder.newColumn('identification').withTitle('C&eacute;dula').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('code').withTitle('Código').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('names').withTitle('Nombres').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('surnames').withTitle('Apellidos').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('birthProvince').withTitle('Provincia Nacimiento').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('birthCity').withTitle('Ciudad Nacimiento').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('birthday').withTitle('Cumpleaños').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('addressCity').withTitle('Ciudad Domicilio').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('address').withTitle('Dirección').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('telephones').withTitle('Teléfono').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('cellphones').withTitle('Celular').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('emails').withTitle('Email').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('gender').withTitle('Género').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('disability').withTitle('Tiene Discapacidad').renderWith(function (data) {
-        var result = (data === true ? 'Si' : 'No');
-        return result;
-      }).withClass('text-right').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('conadis').withTitle('Conadis').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('maritalStatus.name').withTitle('Estado Civil').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('spouseName').withTitle('Conyuge').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('sonNumber').withTitle('# Hijos').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('responsibilities').withTitle('# Cargas').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('paymentMethod').withTitle('Forma de Pago').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('bank.name').withTitle('Banco').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('bankAcountNumber').withTitle('Número Cuenta').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('bankAccountType').withTitle('Tipo de Cuenta').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('grossSalary').withTitle('Sueldo Bruto').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('department.name').withTitle('Departamento').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('office.name').withTitle('Cargo').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('profession').withTitle('Profesión').withOption('defaultContent', ''),
-      DTColumnBuilder.newColumn('status').withTitle('Estado').withOption('defaultContent', '')
-    ];
+    $scope.tableColumns = optionsDataTable.createTableColumns([
+      {field: 'identification', title: 'Identificación'},
+      {field: 'code', title: 'Código'},
+      {field: 'names', title: 'Nombre'},
+      {field: 'surnames', title: 'Apellido'},
+      {field: 'maritalStatus.name', title: 'Estado Marital'},
+      {field: 'birthday', title: 'Fecha Nacimiento', filter: 'date'},
+      {field: 'telephones', title: 'Telefonos'},
+      {field: 'cellphones', title: 'Celulares'},
+      {field: 'emails', title: 'Correo'},
+      {field: 'grossSalary', title: 'Salario'},
+      {field: 'department.name', title: 'Departamento'},
+      {field: 'office.name', title: 'Cargo'},
+      {field: 'status', title: 'Estado'}
+    ]);
 
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-      .withOption('ajax', {
-        url: 'employeeForTable',
-        type: 'POST',
-        headers: {'X-CSRF-Token': CSRF_TOKEN}
-      })
+    $scope.dtOptions = DTOptionsBuilder
+      .fromSource(optionsDataTable.fromSource('employeeForTable'))
       .withDataProp('data')
       .withOption('serverSide', true)
-      .withOption('rowCallback', function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-      $('td', nRow).unbind('click');
-      $('td', nRow).bind('dblclick', function () {
-        $scope.$apply(function () {
-          editEmployee(aData);
-        });
-      });
-      return nRow;
-    })
-      .withOption('order', [2, 'asc'])
-      .withColVis()
-      .withColVisOption('buttonText' , 'Selecionar Columnas')
-      .withOption('stateSave', true)
+      .withOption('rowCallback', optionsDataTable.rowCallback(editEmployee))
       .withOption('iDisplayLength', 25)
       .withOption('deferRender', true)
-      .withOption('scrollY', optionsDataTable.scrollY70)
-      .withOption('scrollX', true)
-      .withTableTools(optionsDataTable.urlTableTools)
-      .withTableToolsButtons([
-        {
-          "sExtends":    "div",
-          "sDiv":        "copy",
-          "sButtonText": "PDF",
-          'sButtonClass': 'btn btn-white btn-sm',
-          "fnClick": function (nButton, oConfig, oFlash) {
-            dataTablePDFMaker.make('A4', 'landscape', 'Resumen Empleados - ' + moment().format('YYYY-MM-DD HH:mm:ss'),
-              'resumen_empleados_' + moment().format('YYYYMMDD_HHmmss'),
-              tableInstance.DataTable.context[0].aoColumns,
-              tableInstance.dataTable.fnGetData(),
-              'file'
-            );
-          }
-        },
-        {
-          "sExtends":    "div",
-          "sDiv":        "copy",
-          "sButtonText": "Excel",
-          'sButtonClass': 'btn btn-white btn-sm',
-          "fnClick": function (nButton, oConfig, oFlash) {
-            dataTableXLSXMaker.make(
-              'Resumen Empleados',
-              'resumen_empleados_' + moment().format('YYYYMMDD_HHmmss'),
-              tableInstance.DataTable.context[0].aoColumns,
-              tableInstance.dataTable.fnGetData()
-            );
-          }
-        },
-        {
-          'sExtends': 'csv',
-          'sButtonText': 'CSV',
-          'sButtonClass': 'btn btn-white btn-sm',
-          'sFileName': fileName + '.csv'
-        },
-        {
-          "sExtends":    "div",
-          "sDiv":        "copy",
-          "sButtonText": "Imprimir",
-          'sButtonClass': 'btn btn-white btn-sm',
-          "fnClick": function (nButton, oConfig, oFlash) {
-            dataTablePDFMaker.make('A4', 'landscape', 'Resumen Empleados - ' + moment().format('YYYY-MM-DD HH:mm:ss'),
-              'resumen_empleados_' + moment().format('YYYYMMDD_HHmmss'),
-              tableInstance.DataTable.context[0].aoColumns,
-              tableInstance.dataTable.fnGetData(),
-              'print'
-            );
-          }
-        }
-      ]);
+      .withOption('scrollY', optionsDataTable.scrollY80)
+      .withOption('dom', optionsDataTable.dom)
+      .withOption('bProcessing', true)
+      .withOption('buttons', optionsDataTable.buttons(fileName, title, 'landscape','A4'))
+      .withOption("stateSave", true)
+      .withOption('stateSaveCallback', optionsDataTable.saveState(title))
+      .withOption('stateLoadCallback', optionsDataTable.loadState(title));
 
     var reloadData = function(){
       tableInstance.reloadData();
@@ -151,22 +88,6 @@ angular.module('app').controller('EmployeesReportCtrl', [
 
     $scope.getTableInstance = function(dtInstance){
       tableInstance = dtInstance;
-    };
-
-    var editEmployee = function (selectedEmployee) {
-      var modalInstance = $modal.open({
-        templateUrl: '../../views/humanResources/employees/employeDetails.html',
-        controller: 'EditEmployeeCtrl',
-        windowClass: 'xlg',
-        resolve: {
-          selectedEmployee: function () {
-            return selectedEmployee;
-          }
-        }
-      });
-      modalInstance.result.then(function () {
-        reloadData();
-      });
     };
 
     $scope.maximize = function () {
