aeafff234cedc01785db8119bc2efc6d55ddce99
digna marquez <marquezdigna83@gmail.com>
2015-12-23 22:32:20 -0700

    El combo de tipo de bono o descuento alimenta el combo del nombre

--- public/js/controllers/humanResources/massiveBonusDiscountLoad/new.js
+++ public/js/controllers/humanResources/massiveBonusDiscountLoad/new.js
@@ -6,11 +6,33 @@ angular.module('app').controller('MassiveBonusDiscountLoadCtrl', [
   function ($scope, documentValidate, server) {
 
     $scope.massiveBonus = {};
-    $scope.departments = {};
+    $scope.departments = [];
+    $scope.bonus = [];
+
+    var typeBonus = '';
 
     server.getAll('departments').success(function (data) {
       $scope.departments = data;
     });
+
+    $scope.searchTypeBonus = function () {
+      if($scope.massiveBonus.typeBonus == 'bonus')
+        typeBonus = 'bonus';
+
+      if($scope.massiveBonus.typeBonus == 'discounts')
+        typeBonus = 'discounts';
+
+
+      if(typeBonus){
+        server.getAll(typeBonus).success(function (data) {
+          $scope.type = data;
+        });
+      }
+    };
+
+
+
+
 
 
     handlePanelAction();
