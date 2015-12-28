97c67cbe8a0e5d6d206ce04172fee0056da8fb6d
digna marquez <marquezdigna83@gmail.com>
2015-12-28 13:25:56 -0700

    Carga de archivo excel

--- public/js/controllers/humanResources/massiveArrearsLoad/new.js
+++ public/js/controllers/humanResources/massiveArrearsLoad/new.js
@@ -4,7 +4,8 @@ angular.module('app').controller('MassiveArrearsLoadCtrl', [
   'documentValidate',
   'server',
   'fileReader',
-  function ($scope, documentValidate, server, fileReader) {
+  'XLSXReaderService',
+  function ($scope, documentValidate, server, fileReader, XLSXReaderService) {
 
     $scope.massiveArrears = {};
     $scope.massiveArrears.prueba= {datos: 'mi prueba'};
@@ -13,11 +14,41 @@ angular.module('app').controller('MassiveArrearsLoadCtrl', [
     $scope.openFile = function() {
       alert('entro');
       console.log($scope.massiveArrears.myFile);
-      fileReader.readFile($scope.massiveArrears.myFile);
+      //fileReader.getReader($scope.massiveArrears.myFile);
     };
 
+    $scope.showPreview = false;
+    $scope.showJSONPreview = true;
+    $scope.json_string = "";
 
 
-    handlePanelAction();
+    $scope.fileChanged = function(files) {
+      $scope.isProcessing = true;
+      $scope.sheets = [];
+      $scope.excelFile = files[0];
+      XLSXReaderService.readFile($scope.excelFile, $scope.showPreview, $scope.showJSONPreview).then(function(xlsxData) {
+        $scope.sheets = xlsxData.sheets;
+        $scope.isProcessing = false;
+      });
+    }
+
+    $scope.updateJSONString = function() {
+      $scope.json_string = JSON.stringify($scope.sheets[$scope.selectedSheetName], null, 2);
+    }
+
+    $scope.showPreviewChanged = function() {
+      if ($scope.showPreview) {
+        $scope.showJSONPreview = false;
+        $scope.isProcessing = true;
+        XLSXReaderService.readFile($scope.excelFile, $scope.showPreview, $scope.showJSONPreview).then(function(xlsxData) {
+          $scope.sheets = xlsxData.sheets;
+          $scope.isProcessing = false;
+        });
+      }
+    }
+
+
+
+   // handlePanelAction();
   }
 ]);
