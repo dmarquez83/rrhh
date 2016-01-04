06e11c3b84d4004728d79fbbefa1344adba9722c
jhefigue <jheizzi.figueroa@gmail.com>
2016-01-03 23:15:34 -0430

    Pantallas PreLiquidacion Rol....trabajando en el modal Rol de Pago y pendiente validar funcionalidad

--- public/js/controllers/humanResources/rolLiquidation/new.js
+++ public/js/controllers/humanResources/rolLiquidation/new.js
@@ -1,9 +1,47 @@
 'use strict';
 angular.module('app').controller('RolLiquidationCtrl', [
   '$scope',
+  '$modal',
   'server',
   function ($scope, server) {
 
+    $scope.openDptoEmployeModal = function () {
+      var modalInstance = $modal.open({
+        templateUrl: '../../views/humanResources/rolLiquidation/dptoEmployeSelections.html',
+        controller: 'RolLiquidationCtrl',
+        size: 'lg',
+        resolve: {
+          Id_Depart: function() //scope del modal
+          {
+            //console.log($scope.massiveBonus.department_id);
+            return $scope.rolLiquidation.department_id;
+
+          }
+        }
+      });
+      modalInstance.result.then(function () {
+        $window.location.reload();
+      });
+    };
+
+    $scope.openPreLiquidarModal = function () {
+      var modalInstance = $modal.open({
+        templateUrl: '../../views/humanResources/rolLiquidation/employePreLiquidados.html',
+        controller: 'RolLiquidationCtrl',
+        size: 'lg',
+        resolve: {
+          Id_Depart: function() //scope del modal
+          {
+            //console.log($scope.massiveBonus.department_id);
+            return $scope.rolLiquidation.department_id;
+
+          }
+        }
+      });
+      modalInstance.result.then(function () {
+        $window.location.reload();
+      });
+    };
 
     handlePanelAction();
   }
