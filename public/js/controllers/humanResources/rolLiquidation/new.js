d87b06bada60c57fed89e1c06f4f9aa7db8a4c1b
jhefigue <jheizzi.figueroa@gmail.com>
2016-01-05 03:50:47 -0430

    algunas pruebas...

--- public/js/controllers/humanResources/rolLiquidation/new.js
+++ public/js/controllers/humanResources/rolLiquidation/new.js
@@ -5,28 +5,29 @@ angular.module('app').controller('RolLiquidationCtrl', [
   'server',
   function ($scope,$modal,server) {
       $scope.rolLiquidation = {};
+      $scope.rolLiquidation.departmentName = '';
       $scope.departments = [];
       $scope.employeSelections = [];
+      var modalInstance;
+
 
       $scope.$on('employees', function (event, values) {
           $scope.employeSelections = values.employeSelections;
       });
-
-      $scope.employeSelections =  $scope.employeSelections;
 
       server.getAll('departments').success(function (data) {
           $scope.departments = data;
       });
 
       $scope.openDptoEmployeModal = function () {
-      var modalInstance = $modal.open({
+        modalInstance = $modal.open({
         templateUrl: '../../views/humanResources/rolLiquidation/dptoEmployeSelections.html',
         controller: 'RolLiquidationCtrl',
         size: 'lg',
         resolve: {
           Id_Depart: function() //scope del modal
           {
-            console.log($scope.rolLiquidation.department_id);
+            //console.log($scope.rolLiquidation.department_id);
             return $scope.rolLiquidation.department_id;
           }
         }
@@ -36,23 +37,80 @@ angular.module('app').controller('RolLiquidationCtrl', [
       });
     };
 
-
       var listEmployees = function(){
          server.post('getEmployees').success(function(result){
-
-              if($scope.rolLiquidation.department_id)
+              if($scope.rolLiquidation.department_id){
                   $scope.employees = _(result).where({ 'department_id':  $scope.rolLiquidation.department_id });
+                 // $scope.rolLiquidation.departmentName = _($scope.departments).findWhere({ '_id': $scope.rolLiquidation.department_id }).name;
+              }
               else
+              {
                   $scope.employees = result;
-          })
+                 // $scope.rolLiquidation.departmentName = _($scope.departments).findWhere({ '_id': $scope.employees.department_id }).name;
+              }
+
+         })
       }
 
       listEmployees();
 
 
+      $scope.saveEmploye = function(){
 
-    $scope.openPreLiquidarModal = function () {
-      var modalInstance = $modal.open({
+          if($scope.employeSelections){
+              angular.forEach($scope.employeSelections, function (employee) {
+
+                  //console.log(employee);
+
+                  var index = 0;
+
+                  /*angular.forEach($scope.massiveBonus.typeBonus, function (type) {
+
+
+                      if(type == 'discounts'){
+                          employee.discounts = _(employee).has('discounts') ? employee.discounts : [];
+                          $scope.assignedDiscounts = angular.copy($scope.massiveBonus.type[index]);
+                          //$scope.assignedDiscounts.date = moment().format();
+                          $scope.assignedDiscounts.frequency = $scope.massiveBonus.frequencyBonus[index];
+                          employee.discounts.push($scope.assignedDiscounts);
+                          //employee.discounts.push($scope.massiveBonus.frequencyBonus[index]);
+                          var discounts = { 'discounts': angular.copy(employee.discounts) };
+                          //console.log(discounts);
+                          server.update('employee', discounts, employee._id).success(function (data) {
+                              toastr[data.type](data.msg);
+                          });
+                      }
+
+                      if(type == 'bonus'){
+                          employee.bonus = _(employee).has('bonus') ? employee.bonus : [];
+                          $scope.assignedBonus = angular.copy($scope.massiveBonus.type[index]);
+                          //$scope.assignedBonus.date = moment().format();
+                          $scope.assignedBonus.frequency = $scope.massiveBonus.frequencyBonus[index];
+                          employee.bonus.push($scope.assignedBonus);
+                          //employee.bonus.push($scope.massiveBonus.frequencyBonus[index]);
+                          var bonus = { 'bonus': angular.copy(employee.bonus) };
+                          //console.log(bonus);
+                          server.update('employee', bonus, employee._id).success(function (data) {
+                              toastr[data.type](data.msg);
+                          });
+
+                      }
+
+                      index++;
+
+                  });*/
+
+              });
+
+          }else {
+              toastr.warning('Debe seleccionar al Menos un Empleado');
+          }
+
+      };
+
+
+      $scope.openPreLiquidarModal = function () {
+        modalInstance = $modal.open({
         templateUrl: '../../views/humanResources/rolLiquidation/employePreLiquidados.html',
         controller: 'RolLiquidationCtrl',
         size: 'xlg',
@@ -80,6 +138,22 @@ angular.module('app').controller('RolLiquidationCtrl', [
           //buscar todos los empleados existentes y con estatus activo
     };
 
+
+    $scope.cancel = function () {
+          modalInstance.close();
+
+    };
+
+      /*var ModalInstanceCtrl = function ($scope,$modalInstance) {
+
+          $scope.ok = function(){
+              $modalInstance.close();
+          }
+
+          $scope.cancel = function(){
+              $modalInstance.dismiss('cancel');
+          }
+      };*/
 
 
     handlePanelAction();
