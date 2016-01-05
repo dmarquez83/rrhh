0ef355feaa51992a4598b733dc391d6821f2cec9
jhefigue <jheizzi.figueroa@gmail.com>
2016-01-05 02:05:22 -0430

    Ya lista los empleados por departamento y por defecto aparecen seleccionados (pto 5)....trabajando en cantidad de empleados seleccionados

--- public/js/controllers/humanResources/rolLiquidation/new.js
+++ public/js/controllers/humanResources/rolLiquidation/new.js
@@ -4,8 +4,21 @@ angular.module('app').controller('RolLiquidationCtrl', [
   '$modal',
   'server',
   function ($scope,$modal,server) {
+      $scope.rolLiquidation = {};
+      $scope.departments = [];
+      $scope.employeSelections = [];
 
-    $scope.openDptoEmployeModal = function () {
+      $scope.$on('employees', function (event, values) {
+          $scope.employeSelections = values.employeSelections;
+      });
+
+      $scope.employeSelections =  $scope.employeSelections;
+
+      server.getAll('departments').success(function (data) {
+          $scope.departments = data;
+      });
+
+      $scope.openDptoEmployeModal = function () {
       var modalInstance = $modal.open({
         templateUrl: '../../views/humanResources/rolLiquidation/dptoEmployeSelections.html',
         controller: 'RolLiquidationCtrl',
@@ -13,9 +26,8 @@ angular.module('app').controller('RolLiquidationCtrl', [
         resolve: {
           Id_Depart: function() //scope del modal
           {
-            //console.log($scope.massiveBonus.department_id);
-            return $scope.department_id;
-
+            console.log($scope.rolLiquidation.department_id);
+            return $scope.rolLiquidation.department_id;
           }
         }
       });
@@ -23,6 +35,20 @@ angular.module('app').controller('RolLiquidationCtrl', [
         $window.location.reload();
       });
     };
+
+
+      var listEmployees = function(){
+         server.post('getEmployees').success(function(result){
+
+              if($scope.rolLiquidation.department_id)
+                  $scope.employees = _(result).where({ 'department_id':  $scope.rolLiquidation.department_id });
+              else
+                  $scope.employees = result;
+          })
+      }
+
+      listEmployees();
+
 
 
     $scope.openPreLiquidarModal = function () {
@@ -48,6 +74,12 @@ angular.module('app').controller('RolLiquidationCtrl', [
 
       //buscar si hay liquidaciones en el mes/quincena seleccionada y arrojar mensaje si ya fue hecha
     };
+
+    $scope.searchEmployeAct = function () {
+
+          //buscar todos los empleados existentes y con estatus activo
+    };
+
 
 
     handlePanelAction();
