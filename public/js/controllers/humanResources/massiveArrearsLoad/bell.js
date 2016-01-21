e51e7c1506185b5ce07a7c6ca76063973d9ab1fe
Luis Hernández <luelher@gmail.com>
2016-01-21 16:24:31 -0430

    Se solventó errores en timbrado, Mejoras en caraga masiva de atrasos. Fixed #3

--- public/js/controllers/humanResources/massiveArrearsLoad/bell.js
+++ public/js/controllers/humanResources/massiveArrearsLoad/bell.js
@@ -9,115 +9,47 @@ angular.module('app').controller('BellCtrl', [
         $scope.serverProcess = false;
         $scope.isUpdate = false;
         $scope.bells = [];
+        $scope.bells_for_save = [];
         $scope._id = [];
         $scope.countBell =[];
         $scope.hourBell= [];
         $scope.typeBell= [];
-        $scope.prueba=false;
+
+
+        server.post('getBells').success(function(result){
+
+            $.each(result, function( index, bell ) {
+              result[index].hourBell = new Date(1970, 0, 1, parseInt(bell.hourBell.substring(0,2)), parseInt(bell.hourBell.substring(3,5)), 0);
+            });
+
+            $scope.bells = result;
+
+            if($scope.bells.length==0){
+                $scope.addBell();
+            }
+        });
 
 
         $scope.addBell = function() {
-            $scope.bells.push({ hecho: true });
+            $scope.bells.push({ hourBell: new Date(1970, 0, 1, 0, 0, 0)});
         };
 
-        $scope.deleteBell = function(index){
-            $scope.prueba=true;
+        $scope.deleteBell = function(index, bell){
             $scope.bells.splice(index,1);
-            var indice =0;
-            angular.forEach(($scope.bells), function(row){
-                $scope._id[indice] = row._id;
-                $scope.countBell[indice] = row.countBell;
-                $scope.hourBell[indice] = row.hourBell;
-                $scope.typeBell[indice] = row.typeBell;
-                indice++;
-            });
-            var idBell = $scope._id[index];
-            if (idBell){
-                eliminar(idBell);
-            }
-        };
-
-        var eliminar = function ($id) {
-            server.delete('bells',$id).success(function(result){
-                if(result.type == 'success') {
-                    toastr[result.type](result.msg);
-                }
-            });
-        };
-
-        $scope.cleanBell = function() {
-            $scope._id = null;
-            $scope.countBell = [];
-            $scope.hourBell= [];
-            $scope.typeBell= [];
-        };
-
-        if(!$scope.prueba){
-            server.post('getBells').success(function(result){
-                $scope.bells = (result);
-                var indice =0;
-                angular.forEach(($scope.bells), function(row){
-                    $scope._id[indice] = row._id;
-                    $scope.countBell[indice] = row.countBell;
-                    $scope.hourBell[indice] = row.hourBell;
-                    $scope.typeBell[indice] = row.typeBell;
-                    indice++;
-                });
-
-                $scope.cuenta = $scope.bells.length;
-            });
-        }
-
-
-        var validateCountBell = function(){
-            if ($scope.countBell.length == 0){
-                toastr.warning('Debe ingresar el contador');
-                return false;
-            }
-            return true;
-        };
-
-        var validateHourBell = function(){
-            if ($scope.hourBell.length == 0){
-                toastr.warning('Ingrese la hora');
-                return false;
-            }
-            return true;
-        };
-
-        var validateTypeBell = function(){
-            if ($scope.typeBell.length == 0){
-                toastr.warning('Seleccione el tipo');
-                return false;
-            }
-            return true;
-        };
-
-        var validate = function(){
-            if (validateCountBell() && validateHourBell() && validateTypeBell()){
-                return true;
-            }
-            return false;
+            if($scope.bells.length==0){
+                $scope.addBell();
+            }            
         };
 
         $scope.save= function () {
-            $scope.bellInfo_=[];
-            $scope.bellInfo = {};
-            $scope.bellInfo.countBell = '';
-            $scope.bellInfo.hourBell = '';
-            $scope.bellInfo.typeBell = '';
 
-            var index = 0;
-            angular.forEach($scope.countBell, function () {
-                $scope.bellInfo.countBell =  parseInt($scope.countBell[index]);
-                $scope.bellInfo.hourBell  =  $scope.hourBell[index];
-                $scope.bellInfo.typeBell  =  $scope.typeBell[index];
-                $scope.bellInfo_.push($scope.bellInfo);
-                $scope.bellInfo = {};
-                index++;
-            } );
+            angular.copy($scope.bells, $scope.bells_for_save);
 
-            server.save('bells',$scope.bellInfo_).success(function (data) {
+            $.each($scope.bells_for_save, function( index, bell ) {
+              $scope.bells_for_save[index].hourBell = bell.hourBell.toTimeString("HH:mm").substring(0,5);
+            });
+
+            server.save('bells',$scope.bells_for_save).success(function (data) {
                 toastr[data.type](data.msg);
             });
         };
